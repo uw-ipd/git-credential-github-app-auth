67ec29d996e633efdf1662bb5c5fa2d3774797c9
Alex Ford <fordas@uw.edu>
2018-06-13 18:03:21 -0700

    Add initial travis and tox configuration.

--- setup.py
+++ setup.py
@@ -2,9 +2,33 @@ from setuptools import setup
 
 setup(
     name="git-credential-github-app-auth",
-    version='0.1',
+    author="Alex Ford",
+    author_email='fordas@uw.edu',
+    license="MIT license",
+    description="A git-credential helper for https access via GitHub Apps.",
+    url='https://github.com/uw-ipd/git-credential-github-app-auth',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    version='0.1.0',
+
     py_modules=['git_credential_github_app_auth'],
-    python_requires='>=3.5',
+    entry_points={
+        'console_scripts' : [
+        'git-credential-github-app-auth='
+            'git_credential_github_app_auth.cli:cli',
+        ]
+    },
     install_requires=[
         'click',
         'PyJWT',
@@ -12,9 +36,8 @@ setup(
         'cryptography',
         'attrs',
     ],
+
     test_requires=["pytest"],
-    entry_points='''
-        [console_scripts]
-        git-credential-github-app-auth=git_credential_github_app_auth.cli:cli
-    ''',
+
+    zip_safe=False,
 )
